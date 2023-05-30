a71f549c3b8bd10acd31d0728676371c7f13eab4
Aiyen311 <adithya_iyengar@hotmail.com>
2023-05-29 22:19:54 -0400

    added sample data and initial tweak

--- website/main.py
+++ website/main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import psycopg2
 import aiosql
 import os
+import csv
 from pydantic import BaseModel
 
 
@@ -15,28 +16,34 @@ class Company(BaseModel):
     name: str
     address: str
 
+def read_csv(file_path):
+    with open(file_path, 'r') as csv_file:
+        reader = csv.DictReader(csv_file)
+        data = list(reader)
+    return data    
 
-@app.get("/hello/{name}")
-async def root(request: Request, name: str):
-    accept = request.headers.get("accept")
-    hello_world = {"message": f"hello {name}"}
+# @app.get("/hello/{name}")
+# async def root(request: Request, name: str):
+#     accept = request.headers.get("accept")
+#     hello_world = {"message": f"hello {name}"}
 
-    if accept.split("/")[1] == 'json':
-        return hello_world
+#     if accept.split("/")[1] == 'json':
+#         return hello_world
 
-    if len(accept.split(",")) > 1 or accept.split("/")[1] == 'html':
-        response = templates.TemplateResponse("index.html", {"request": request, "payload1": hello_world}) 
-        return response
+#     if len(accept.split(",")) > 1 or accept.split("/")[1] == 'html':
+#         response = templates.TemplateResponse("index.html", {"request": request, "payload1": hello_world}) 
+#         return response
 
 @app.get("/companies")
 async def get_companies(request: Request):
+    file_path = "sample-data/project.csv"  
+    data = read_csv(file_path)
     load_dotenv()
 
-    conn = psycopg2.connect(f"dbname=tdm_main user=rdonly password={os.getenv('PG_PASSWORD')} host=db.tdm.geddes.rcac.purdue.edu")
-    queries = aiosql.from_path("queries.sql", "psycopg2")
+    # conn = psycopg2.connect(f"dbname=tdm_main user=iyengar1 password=FOLD-gumption-aqualung-slurry-weary host=lpvtdmdb01.itap.purdue.edu")
+    # queries = aiosql.from_path("queries.sql", "psycopg2")
 
-    companies = [v[0] for v in queries.get_all_companies(conn)]
-
+    companies = [v['company'] for v in data]
     #companies = [Company(name=v) for v in companies]
 
     print(f"{companies=}")
