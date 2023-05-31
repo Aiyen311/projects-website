536c5b3485f42b2b9d132ee28e364d13078df1ef
Aiyen311 <adithya_iyengar@hotmail.com>
2023-05-31 16:09:19 -0400

    first level commit, created card stack layout with mock data

--- main.py
+++ main.py
@@ -22,6 +22,12 @@ def read_csv(file_path):
         data = list(reader)
     return data    
 
+def get_projects():
+    file_path = "sample_data/projects.csv"  
+    data = read_csv(file_path)
+    companies = [v for v in data]
+    return companies
+
 # @app.get("/hello/{name}")
 # async def root(request: Request, name: str):
 #     accept = request.headers.get("accept")
@@ -36,24 +42,29 @@ def read_csv(file_path):
 
 @app.get("/companies")
 async def get_companies(request: Request):
-    file_path = "sample-data/project.csv"  
-    data = read_csv(file_path)
+    projects = get_projects()
     load_dotenv()
 
     # conn = psycopg2.connect(f"dbname=tdm_main user=iyengar1 password=FOLD-gumption-aqualung-slurry-weary host=lpvtdmdb01.itap.purdue.edu")
     # queries = aiosql.from_path("queries.sql", "psycopg2")
 
-    companies = [v['company'] for v in data]
-    #companies = [Company(name=v) for v in companies]
-
-    print(f"{companies=}")
-
     accept = request.headers.get("accept")
 
     if accept.split("/")[1] == 'json':
-        return companies
+        return projects
 
     if len(accept.split(",")) > 1 or accept.split("/")[1] == 'html':
-        response = templates.TemplateResponse("test_templates/companies.html", {"request": request, "payload1": companies}) 
+        response = templates.TemplateResponse("test_templates/companies.html", {"request": request, "payload1": projects}) 
         return response
 
+@app.get("/home")
+async def get_index(request: Request):
+    response = templates.TemplateResponse("base.html", {"request": request}) 
+    return response
+
+@app.get("/list")
+async def get_companies(request: Request):
+    projects = get_projects()
+
+    response = templates.TemplateResponse("test_templates/list.html", {"request": request, "payload1": projects}) 
+    return response
